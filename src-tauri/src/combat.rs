194e48439a2f79d414e17455f49016042e1ff738
Andrew Francis <ajfrancis102@gmail.com>
2022-05-13 18:50:41 -0400

    Implemented Combat scaffolding

--- src-tauri/src/combat.rs
+++ src-tauri/src/combat.rs
@@ -1,7 +1,6 @@
 use crate::entity::Entity;
 
 fn resolve_combat(character: &Entity, enemy: &Entity) -> Vec<CombatStep> {
-
     let mut combat_steps = Vec::new();
 
     let character_attack_power = character.strength * 10;
@@ -10,14 +9,13 @@ fn resolve_combat(character: &Entity, enemy: &Entity) -> Vec<CombatStep> {
     let enemy_attack_power = enemy.strength * 10;
     let mut enemy_health = enemy.stamina * 10;
 
-    loop{
+    loop {
         enemy_health -= character_attack_power;
 
         if enemy_health <= 0 {
-
-            combat_steps.push(CombatStep{
+            combat_steps.push(CombatStep {
                 character_damage: character_attack_power,
-                enemy_damage: 0
+                enemy_damage: 0,
             });
 
             return combat_steps;
@@ -25,15 +23,14 @@ fn resolve_combat(character: &Entity, enemy: &Entity) -> Vec<CombatStep> {
 
         character_health -= enemy_attack_power;
 
-        combat_steps.push(CombatStep{
+        combat_steps.push(CombatStep {
             character_damage: character_attack_power,
-            enemy_damage: enemy_attack_power
-        })
+            enemy_damage: enemy_attack_power,
+        });
 
-        if character_health <= 0{
+        if character_health <= 0 {
             return combat_steps;
         }
-
     }
 }
 
